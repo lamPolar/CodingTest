1e321b26905b2d7c2481b20de327a89226097c19
lamPolar <lovast7@gmail.com>
2022-07-23 18:38:06 +0900

    solved #11279, #11286, #1715, #1927, #2075

---
+++ BaekJoon/11279.py
@@ -0,0 +1,14 @@
+import heapq
+import sys
+
+heap = [] 
+N = int(input())
+for _ in range(N):
+    cmd = int(sys.stdin.readline())
+    if (cmd > 0):
+        heapq.heappush(heap, (-cmd, cmd))
+    else:
+        if (len(heap)):
+            print(heapq.heappop(heap)[1])
+        else : 
+            print(0)
